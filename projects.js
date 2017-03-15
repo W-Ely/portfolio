7bd16ce6f054006775c1d8d9bdd6aceea27a2481
W. Ely Paysinger <paysinger@gmail.com>
2017-03-15 08:15:23 -0700

    formating and content changes, line spacings

---
+++ projects.js
@@ -0,0 +1,19 @@
+'use strict';
+
+var projectData = [
+  {
+    name: 'test1',
+    link: 'http://www.testmy.net',
+    summery: 'ipsum lots o text'
+  },
+  {
+    name: 'test2',
+    link: 'http://www.testmy.net',
+    summery: 'ipsum lots o text'
+  },
+  {
+    name: 'test3',
+    link: 'http://www.testmy.net',
+    summery: 'ipsum lots o text'
+  }
+]
