ef743b799144b5bafd9a45f7b5b23d5c4dadc6a7
W. Ely Paysinger <paysinger@gmail.com>
2017-03-30 08:27:38 -0700

    removed corpse nav code. page appears to function as intended.

---
+++ public/scripts/view/nav.js
@@ -0,0 +1,91 @@
+'use strict';
+(function(module){
+
+  const nav = {
+    $clickedTab: undefined,
+    state: {
+      //these flags help pick and rest nav functionality.
+      mobile: true,
+      dropNav: false,
+      touch: false,
+    },
+  };
+
+  nav.getState = function(){
+    if ( $(window).width() < 777 ) {
+      nav.state.mobile = true;
+    } else if ( $(window).width() >= 777) {
+      nav.state.mobile = false;
+    }
+  };
+
+  nav.setEvents = function(){
+    // this handles screen size changes. Perhaps a phone going to landscape, or more likely a tester draging the edge on window.
+    $(window).on('resize', function(){
+      nav.getState();
+      nav.state.touch = false;
+      if (!nav.state.mobile){
+        $('.side-menu').find('ul').show();
+      } else {
+        $('.side-menu').find('ul').hide();
+      }
+    });
+    $('.side-menu').on('touchstart', function(){
+      nav.state.touch = true;
+    });
+    $('.side-menu').on('click', function(){
+      if (nav.state.mobile){
+        nav.state.dropNav = !nav.state.dropNav;
+        $(this).find('ul').toggle();
+      }
+    });
+    $('.side-menu').on('mouseleave', function(){
+      if (nav.state.mobile) {
+        nav.state.dropNav = false;
+        $('.side-menu').find('ul').hide();
+      }
+    });
+    $('.side-menu').on('mouseenter', function(){
+      if (!nav.state.dropNav && nav.state.mobile && !nav.state.touch){
+        nav.state.dropNav = true;
+        $('.side-menu').find('ul').show();
+      }
+    });
+  };
+
+  nav.setMenu = function() {
+    $('section').hide();
+    $('#landing').show();
+  }
+
+  nav.setProjectArrows = function(){
+    var distanceLeftCSS = 0;
+    $('.project-nav').on('click', '.arrow', function(){
+      if ($(this).attr('id') === 'forward' && distanceLeftCSS > ($('article').length - 1) * -100){
+        //take us forward
+        distanceLeftCSS -= 100;
+        $('.projects-carousel').animate({
+          left: distanceLeftCSS + 'vw',
+        }, 500 );
+      } else if ($(this).attr('id') === 'back' && distanceLeftCSS < 0) {
+        //take us back
+        distanceLeftCSS += 100;
+        $('.projects-carousel').animate({
+          left: distanceLeftCSS + 'vw',
+        }, 500 );
+      }
+    });
+  }
+
+  nav.preparePageNavigation = () => {
+    nav.getState();
+    nav.setMenu();
+    nav.setEvents();
+    nav.setProjectArrows();
+  }
+
+  nav.preparePageNavigation();
+
+  module.nav = nav;
+
+}(window))
